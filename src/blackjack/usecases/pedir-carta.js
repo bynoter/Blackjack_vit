771a90ab0ff1da9cbb001ce94e6d9585f208dc10
@bynoter <jcarlostoso@gmail.com>
2023-12-03 00:37:19 -0600

    Se refactoriza main js

---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @param {Array <String>} deck  e sun arreglo de string
+ * @returns  {String} retorna la carta del deck
+ */
+
+export const pedirCarta = (deck) => {
+
+    if ( !deck || deck.length === 0 ) {
+        throw new Error ('No hay cartas en el deck');
+    }
+    const carta = deck.pop();
+    return carta;
+}
